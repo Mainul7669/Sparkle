2c448b139db40c0adb6a0b4f7759bdf44528e5d0
Mainul7669 <mainul7669@gmail.com>
2023-10-09 19:26:03 +0600

    responsiveness and opacity added to second banner

--- src/components/SecondBanner/SecondBanner.jsx
+++ src/components/SecondBanner/SecondBanner.jsx
@@ -26,18 +26,24 @@ const SecondBanner = () => {
   }, [index]);
 
   return (
-    <div className="relative mb-10">
+    <div className="relative mb-10 lg:mt-10">
+    <div className="w-full h-auto lg:h-[600px] relative">
       <img
         src="/public/background-2-scaled.webp"
         alt="Banner Image"
-        className="w-full h-auto lg:h-[600px]"
+        className="w-full h-full object-cover"
       />
+      {/* Blur Overlay */}
+      <div
+        className="absolute inset-0 bg-gray-800 opacity-50 filter"
+      ></div>
+    </div>
       <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
         <div className="text-white text-center">
-          <h3 className="w-fit text-xs font-semibold text-black bg-[#FAC213] px-4 text-center py-2 rounded-full ">
+          <h3 className="w-fit text-xs font-semibold text-black bg-[#FAC213] px-4 text-center py-2 rounded-full mb-2 lg:mb-8 lg:ml-[280px]">
             NEW SEASON
           </h3>
-          <h1 className="text-[26px] font-bold mb-4">
+          <h1 className=" text-xl lg:text-2xl font-bold mb-2 lg:mb-4">
             Back to the past: {text}
           </h1>
           <p className="text-md mb-4">
